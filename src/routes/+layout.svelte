e2b27981687fc2c40823bac008076ed58c999343
harshmangalam <harshdev8218@gmail.com>
2023-08-26 16:24:33 +0530

    added theme dark globally

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,12 @@
 <!-- Leave this. Or you can add more content for your custom layout -->
 <script>
+  import { browser } from "$app/environment";
   import "@fontsource/inter";
+
+  if (browser) {
+    localStorage.setItem("theme", "dark");
+    localStorage.setItem("SVELTEPRESS_DARK_MODE", "on");
+  }
 </script>
 
 <slot />
