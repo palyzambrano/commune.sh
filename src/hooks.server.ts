4307283aab3264027ff92abc0215d61843f77ec3
harshmangalam <harshdev8218@gmail.com>
2023-09-13 19:49:04 +0530

    fix 404 error

---
+++ src/hooks.server.ts
@@ -0,0 +1,10 @@
+import { redirect } from "@sveltejs/kit";
+
+/** @type {import('@sveltejs/kit').HandleServerError} */
+export async function handleError({ error, event }) {
+  if (error instanceof Error) {
+    if (error.message.includes("Not found")) {
+      throw redirect(308, `https://app.commune.sh${event.url.pathname}`);
+    }
+  }
+}
